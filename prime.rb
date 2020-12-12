5dc408ff4656eb266a06b5f1a7cac7978753c2b4
Romano Livi <romano_livi@outlook.com>
2020-12-12 22:41:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 def prime?(num)
   if num <= 1 
-    return false 
-
-
-
-
-
+    return false
+  elsif
+    (2..num -1).each do |i|
+      if num % i == 0 
+        return false 
+      end
+    end
+  end
+true
 end
